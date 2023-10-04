48752355148d2b2fe548daa0b4ae8fc7979fd5ef
Vuong Quoc Nguyen <nguvuong@sheridancollege.ca>
2023-10-04 00:52:02 -0400

    commit first unit testing program

---
+++ UnitTests/UnitTests/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public static bool IsOdd(int value)
+    {
+        return value % 2 == 1;
+    }
+}
+
---
+++ UnitTests/UnitTests/testclass.cs
@@ -0,0 +1,27 @@
+using Xunit;
+
+public class testclass
+{
+
+    [Theory]
+    [InlineData(3)]
+    [InlineData(5)]
+    [InlineData(6)]
+    public void MyFirstTheory(int mynumber)
+    {
+        Assert.True(Program.IsOdd(mynumber));
+    }
+
+
+    [Fact]
+    public void PassingAddTest()
+    {
+        Assert.Equal(4, Program.Add(2, 2));
+    }
+
+    [Fact]
+    public void FailingTest()
+    {
+        Assert.NotEqual(5, Program.Add(2, 2));
+    }
+}
